e2aaece6a21174614445e15125bcc8bee9baa5a6
Cannicakee <stanfill11@gmail.com>
2024-04-13 14:37:06 -0600

    Solved Problem 17

--- aa03-recursion-pt4/problems/17-bonus-permutations.js
+++ aa03-recursion-pt4/problems/17-bonus-permutations.js
@@ -15,9 +15,38 @@ permutations([1, 2, 3]) // [[1, 2, 3], [1, 3, 2],
 ***********************************************************************/
 
 const permutations = (array) => {
-  // Your code here 
-};
+  let newArr = [];
+  if (array.length === 0) return [];
+  if (array.length === 1) return [array];
 
+  for (let i = 0; i < array.length; i++) {
+    let num = array[i]
+    let otherNums = array.slice(0, i).concat(array.slice(i + 1))
+    let permutationsNums = permutations(otherNums);
+    for (let j = 0; j < permutationsNums.length; j++) {
+      let permutationsArray = [num].concat(permutationsNums[j])
+      newArr.push(permutationsArray);
+    }
+  }
+  return newArr;
+};
+// const permutations = arr => {
+//   if (arr.length <= 2) return arr.length === 2 ? [arr, [arr[1], arr[0]]] : arr;
+//   return arr.reduce(
+//     (acc, item, i) =>
+//       acc.concat(
+//         permutations([...arr.slice(0, i), ...arr.slice(i + 1)]).map(val => [
+//           item,
+//           ...val,
+//         ])
+//       ),
+//     []
+//   );
+// };
+console.log(permutations([1, 2])) // [[1, 2], [2, 1]]
+console.log(permutations([1, 2, 3])) // [[1, 2, 3], [1, 3, 2],
+// [2, 1, 3], [2, 3, 1],
+// [3, 1, 2], [3, 2, 1]]
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = permutations;
